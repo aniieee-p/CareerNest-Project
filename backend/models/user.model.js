bde5ba4d883cbd44641a7ee04d7220135b26123b
Anisha <anisha63911@gmail.com>
2026-03-12 00:30:54 +0530

    navbar setup done

---
+++ backend/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phonenumber: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['student', 'recruiter'],
+        required: true
+    },
+    profile:{
+        bio: {type: String},
+        skills: [{type: String}],
+        resume: {type: String}, //url to the resume file
+        resumeOriginalName: {type: String},
+        Company: {type: mongoose.Schema.Types.ObjectId, ref: 'Company'},
+        profilephoto: {
+            default: "",
+            type: String
+        } //url to the profile photo
+    },    
+}); {timestamps: true};
+
+const User = mongoose.model("User", userSchema);
+export default User;
+
+
+
+
