3dc43aa46ad0966015dd0b54f59779650c319c7b
Anisha <anisha63911@gmail.com>
2026-03-13 02:45:41 +0530

    Updated Navbar

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,37 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
-import { Avatar, AvatarImage } from '../ui/avatar'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
-    <div className='bg-white'>
-      <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
         <div>
-          <h1 className='text-2xl font-bold'>Career<span className='text-[#F83002]'>Nest</span></h1>
+          <h1 className="text-2xl font-bold">
+            Career<span className="text-[#5abfce]">Nest</span>
+          </h1>
         </div>
-        <div className='flex items-center gap-12'>
-          <ul className='flex font-medium items-center gap-5'>
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
             <li>Home</li>
             <li>Jobs</li>
             <li>Browse</li>
           </ul>
-        </div>
-        <div>
           <Popover>
             <PopoverTrigger asChild>
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
               </Avatar>
             </PopoverTrigger>
-            <PopoverContent>
-              <h1>Hello </h1>
+            <PopoverContent className="w-80">
+              <div className="flex gap-4 items-center">
+                <Avatar className="cursor-pointer">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                </Avatar>
+                <div>
+                <h4 className="font-medium"> kyra S.</h4>
+                <p className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet.</p>
+                </div>
+              </div>
+
+              <div>
+              <Button variant="Link" className="underline-offset-4 hover:underline">view profile</Button>  
+              <Button variant="Link">Logout</Button>  
+              </div>
             </PopoverContent>
           </Popover>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
