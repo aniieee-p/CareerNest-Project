cf2ea2858d303759b70f17a5f576455e290e7d32
Anisha <anisha63911@gmail.com>
2026-03-26 02:46:59 +0530

    fix: use sameSite=none secure=true for cross-origin cookies

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -104,7 +104,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
+        secure: true,
       })
       .json({
         message: `Welcome back ${user.fullname}`,
@@ -117,7 +118,7 @@ export const login = async (req, res) => {
 };
 export const logout = async (req, res) => {
   try {
-    return res.status(200).cookie("token", "", { maxAge: 0, httpOnly: true, sameSite: "strict" }).json({
+    return res.status(200).cookie("token", "", { maxAge: 0, httpOnly: true, sameSite: "none", secure: true }).json({
       message: "Logged out successfully",
       success: true,
     });
