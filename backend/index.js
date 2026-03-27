524505bc02c93ed41bc0b38de292e8d80a8eb302
Anisha <anisha63911@gmail.com>
2026-03-28 01:18:14 +0530

    fix: update dotenv config for render deployment

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import jobRoute from './routes/job.route.js';
 import applicationRoute from './routes/application.route.js';
 import contactRoute from './routes/contact.route.js';
 
-dotenv.config({});
+dotenv.config();
 
 
 const app = express();
