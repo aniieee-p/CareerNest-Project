7d76ef5cffc3b01fdf29679f5ed03b55b0946d4d
aniieee-p <anisha63911@gmail.com>
2026-08-23 12:15:51 +0530

    added localhost in cors config

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,7 @@ const allowedOrigins = new Set(
     "https://careernest-anisha.netlify.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://localhost", // Android Capacitor
   ].filter(Boolean)
 );
 
